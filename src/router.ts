bd6d63b2c96261d4e53d119d819a09e312ccbc66
Daniel GGuerra <danielgguerra2@gmail.com>
2021-08-20 20:54:03 -0300

    Criando rota para streming de musica

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,29 @@
 import { Router } from "express";
+import { getStaticMusic } from "./utils/getStaticFile";
+import { readMusicFile } from "./utils/readMusicFile";
 
 const router = Router();
-
+/*
 router.get('/api', (req, res) => {
     res.status(200).json({
         status: 200,
         message: 'Server online'
     });
-});
+});*/
+
+router.get('/api/music', async (req, res) => {
+    const dataMusic = await getStaticMusic('medo.mp3');
+
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg3',
+        'Content-Length': dataMusic.size,
+    });
+
+    const streamMusic = readMusicFile('medo.mp3');
+
+    streamMusic.on('end', () => console.log('finish'));
+
+    streamMusic.pipe(res);
+})
 
 export default router;
